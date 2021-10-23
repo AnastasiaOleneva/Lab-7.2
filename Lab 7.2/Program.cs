bfeffd5e6b0ea85290f7b3f62d1e0a420552db9d
79162 <79162@LAPTOP-5NFBVRQ6>
2021-10-23 15:13:19 +0300

    Добавьте файлы проекта.

---
+++ Lab 7.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            double V, S;
+            calcCube(a, out V, out S);
+            Console.ReadKey();
+        }
+
+
+        static void calcCube(int a, out double V, out double S) 
+        {
+            Console.WriteLine("Введите высоту ребра куба");
+            a = Convert.ToInt32(Console.ReadLine());
+            V = Math.Pow(a,3);
+            Console.WriteLine("Объем куба равен {0}",V);
+            S = 6 * Math.Pow(a,2);
+            Console.WriteLine("Площадь куба равна {0}", S);
+            Console.ReadKey();
+        }
+    }
+}
